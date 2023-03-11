82950bc210ad7865a2d7bf9a8dc68c1c7cdfb03d
SumitRoy03 <74671143+SumitRoy03@users.noreply.github.com>
2023-03-11 18:44:03 +0530

    Update openaiRoutes.js

--- routes/openaiRoutes.js
+++ routes/openaiRoutes.js
@@ -3,5 +3,10 @@ const { generateImage } = require('../controllers/openaiController');
 const router = express.Router();
 
 router.post('/generateimage', generateImage);
+router.get('/generateimage', (req,res)=>{
+    res.status(200).json({
+        success:'ok'
+    })
+});
 
 module.exports = router;
